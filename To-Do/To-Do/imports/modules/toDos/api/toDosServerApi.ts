78262a93ba6121d927bc5339b4de187967970c90
Mayke <mayke000000000@gmail.com>
2025-03-27 11:11:19 -0300

    feat: criando publicações

--- To-Do/To-Do/imports/modules/toDos/api/toDosServerApi.ts
+++ To-Do/To-Do/imports/modules/toDos/api/toDosServerApi.ts
@@ -3,6 +3,7 @@ import { Recurso } from '../config/recursos';
 import { toDosSch, IToDos } from './toDosSch';
 import { userprofileServerApi } from '/imports/modules/userprofile/api/userProfileServerApi';
 import { ProductServerBase } from '/imports/api/productServerBase';
+import { IContext } from '/imports/typings/IContext';
 
 // endregion
 
@@ -15,68 +16,94 @@ class ToDosServerApi extends ProductServerBase<IToDos> {
 
 		const self = this;
 
-		this.addTransformedPublication(
-			'toDosList',
-			(filter = {}) => {
-				return this.defaultListCollectionPublication(filter, {
-					projection: { title: 1, type: 1, typeMulti: 1, createdat: 1 }
-				});
-			},
-			(doc: IToDos & { nomeUsuario: string }) => {
-				const userProfileDoc = userprofileServerApi.getCollectionInstance().findOne({ _id: doc.createdby });
-				return { ...doc };
-			}
-		);
+
 
 		this.addPublication('toDosDetail', (filter = {}) => {
 			return this.defaultDetailCollectionPublication(filter, {
 				projection: {
-					contacts: 1,
 					title: 1,
 					description: 1,
-					type: 1,
 					typeMulti: 1,
 					date: 1,
-					files: 1,
-					chip: 1,
-					statusRadio: 1,
-					statusToggle: 1,
-					slider: 1,
-					check: 1,
-					address: 1
+					
 				}
 			});
 		});
 
-		this.addRestEndpoint(
-			'view',
-			(params, options) => {
-				console.log('Params', params);
-				console.log('options.headers', options.headers);
-				return { status: 'ok' };
-			},
-			['post']
-		);
+		// this.addPublication('toDosList', (filter = {}) => {
 
-		this.addRestEndpoint(
-			'view/:toDosId',
-			(params, _options) => {
-				console.log('Rest', params);
-				if (params.toDosId) {
-					return self
-						.defaultCollectionPublication(
-							{
-								_id: params.toDosId
-							},
-							{}
-						)
-						.fetch();
-				} else {
-					return { ...params };
-				}
-			},
-			['get']
-		);
+		// 	console.log(filter);
+		// 	return this.defaultListCollectionPublication(filter, {
+		// 		projection: {
+		// 			title: 1,
+		// 			description: 1,
+		// 			typeMulti: 1,
+		// 			date: 1,
+		// 			userId: 1 
+		// 		}
+		// 	});
+		// });
+		
+		this.addTransformedPublication('toDosList', async (filter = {}) => {
+			return this.defaultListCollectionPublication(filter, {
+			  projection: {
+				title: 1,
+				description: 1,
+				typeMulti: 1,
+				date: 1,
+				userId: 1 
+			  }
+			});
+		  }, async (document: IToDos) => {
+			const user = await userprofileServerApi.findOne({ _id: document.userId });
+		  
+			if (user) {
+			  document.user = {
+				_id: user._id,
+				username: user.username,
+				email: user.email
+			  };
+			} 
+			console.log({...document})
+			return document; 
+		  });
+
+		
+		// this.addRestEndpoint(
+		// 	'view',
+		// 	(params, options) => {
+		// 		console.log('Params', params);
+		// 		console.log('options.headers', options.headers);
+		// 		return { status: 'ok' };
+		// 	},
+		// 	['post']
+		// );
+
+		// this.addRestEndpoint(
+		// 	'view/:toDosId',
+		// 	(params, _options) => {
+		// 		console.log('Rest', params);
+		// 		if (params.toDosId) {
+		// 			return self
+		// 				.defaultCollectionPublication(
+		// 					{
+		// 						_id: params.toDosId
+		// 					},
+		// 					{}
+		// 				)
+		// 				.fetch();
+		// 		} else {
+		// 			return { ...params };
+		// 		}
+		// 	},
+		// 	['get']
+		// );
+	}
+
+	beforeInsert(doc: IToDos, context: IContext): Promise<boolean>{
+		const user = Meteor.userId();
+		if (user) doc.userId = user;
+		return super.beforeInsert(doc,context);
 	}
 }
 
