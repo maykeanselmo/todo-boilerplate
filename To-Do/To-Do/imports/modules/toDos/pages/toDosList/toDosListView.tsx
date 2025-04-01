ca4d67e704ad0be9c58a9f2049667e3db866aa88
Mayke <mayke000000000@gmail.com>
2025-04-01 15:24:16 -0300

    feat: adicionando boão de deleção e edição

--- To-Do/To-Do/imports/modules/toDos/pages/toDosList/toDosListView.tsx
+++ To-Do/To-Do/imports/modules/toDos/pages/toDosList/toDosListView.tsx
@@ -13,7 +13,7 @@ import SysTextField from '/imports/ui/components/sysFormFields/sysTextField/sysT
 import { SysSelectField } from '/imports/ui/components/sysFormFields/sysSelectField/sysSelectField';
 import SysIcon from '/imports/ui/components/sysIcon/sysIcon';
 import { SysDatePickerField } from '/imports/ui/components/sysFormFields/sysDatePickerField/sysDatePickerField';
-import { List } from '@mui/material';
+import { List , Checkbox } from '@mui/material';
 import TaskIcon from '@mui/icons-material/Task';
 import { ToDosModuleContext } from '../../toDosContainer';
 
@@ -23,40 +23,39 @@ import Divider from '@mui/material/Divider';
 import ListItemText from '@mui/material/ListItemText';
 import ListItemAvatar from '@mui/material/ListItemAvatar';
 import Avatar from '@mui/material/Avatar';
+import { SysButton } from '/imports/ui/components/SimpleFormFields/SysButton/SysButton';
+import SysMenuItemDefault from '/imports/ui/components/sysMenu/components/sysMenuItemDefault';
+import { ShowDialog } from '../../../../ui/appComponents/showDialog/showDialog';
 
-
+import { Button } from '@mui/material';
 
 const ToDosListView = () => {
 	const controller = React.useContext(ToDosListControllerContext);
 	const sysLayoutContext = React.useContext(AppLayoutContext);
 	const {id} = React.useContext(ToDosModuleContext);
 	const navigate = useNavigate();
+
   const {
     Container,
     LoadingContainer,
     SearchContainer,
 	TaskContainer
   } = ToDosListStyles;
-
+ 
   const options = [{ value: '', label: 'Nenhum' }, ...(controller?.schema?.type?.options?.() ?? [])];
+  const [checked, setChecked] = React.useState<string[]>([]);
 
+ 
 	return (
 		<Container>
+
 			<Typography variant="h5">Lista de Itens</Typography>
 			<SearchContainer>
 				<SysTextField
 					name="search"
-					placeholder="Pesquisar por nome"
+					placeholder="Procurar tarefa(s)"
 					onChange={controller.onChangeTextField}
 					startAdornment={<SysIcon name={'search'} />}
-				/>
-				<SysSelectField
-					name="Category"
-
-					label="Categoria"
-					options={options}
-					placeholder="Selecionar"
-					onChange={controller.onChangeCategory}
 				/>
 				
 			</SearchContainer>
@@ -67,23 +66,31 @@ const ToDosListView = () => {
 				</LoadingContainer>
 			) : (
 				<TaskContainer>
+				
 				<List>
 				  {controller.todoList.map((task, index) => (
 					<React.Fragment key={task._id}>
-					  <ListItem alignItems="flex-start">
+					  <ListItem alignItems="flex-start" >
 						<ListItemText
 						  primary={
 							<Typography variant="h6" sx={{ fontWeight: 'bold', color: 'primary.main' }}>
-							  {task.description}
+								{task.isCompleted
+        							? <del>{task.title}</del> 
+        							: task.title                
+      						}
 							</Typography>
 						  }
 						  secondary={
 							<Typography variant="body2" color="text.secondary">
-							  Criado por <strong>{task.user?.username || "Desconhecido"}</strong> — {new Date(task.date).toLocaleDateString()}
+							  Criado por: <u>{task.user?.username || "Desconhecido"}</u> — {new Date(task.date).toLocaleDateString()}
 							</Typography>
 						  }
 						/>
-						<SysIcon name={'edit'} onClick={() => navigate('/toDos/edit/' + task._id)}/>
+					
+						<SysIcon name={'edit'} onClick={() => controller.onEditButtonClick(task._id)}/>
+						<SysIcon name={'delete'} onClick={() => controller.onDeleteButtonClick(task)}/>
+						
+						
 					  </ListItem>
 					  {index < controller.todoList.length - 1 && <Divider variant="inset" />}
 					  
@@ -91,11 +98,11 @@ const ToDosListView = () => {
 				  ))}
 				</List>
 			  </TaskContainer>
-			)}
+			)}			
 
 			<SysFab
 				variant="extended"
-				text="Adicionar"
+				text="Adicionar Tarefa"
 				startIcon={<SysIcon name={'add'} />}
 				fixed={true}
 				onClick={controller.onAddButtonClick}
