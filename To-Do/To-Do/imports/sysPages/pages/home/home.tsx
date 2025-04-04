3eef0fda53159b529ce1738efc24816f85aec803
Mayke <mayke000000000@gmail.com>
2025-04-04 15:45:33 -0300

    feat: página welcome

--- To-Do/To-Do/imports/sysPages/pages/home/home.tsx
+++ To-Do/To-Do/imports/sysPages/pages/home/home.tsx
@@ -4,26 +4,48 @@ import HomeSectionNotificacoes from './sections/notificacoes';
 import HomeSectionDialogs from './sections/dialogs';
 import HomeStyles from './homeStyle';
 import HomeSectionComponents from "/imports/sysPages/pages/home/sections/componentTests";
-
+import AuthContext from '/imports/app/authProvider/authContext';
+import { SysFab } from '/imports/ui/components/sysFab/sysFab';
+import SysIcon from '/imports/ui/components/sysIcon/sysIcon';
+import { useNavigate } from 'react-router-dom';
 
 const Home: React.FC = () => {
   const { Container, Header, } = HomeStyles;
+  const authContext = React.useContext(AuthContext);
+
+  const navigate = useNavigate();
+
+  const handleToTaskList = () =>{
+	navigate(`/toDos`);
+  }
+
+  
 
 	return (
 		<Container>
-      <Header>
-				<Typography variant="h3">Página de testes</Typography>
+			<Header>
+				<Typography variant="h1">Olá, {authContext.user?.username}</Typography>
 				<Typography variant="body1" textAlign={'justify'}>
-					Bem vindo ao Boilerplate do Synergia. Essa é uma página dedicada aos testes e exibições de componentes e
-					funcionalidades do nosso sistema. Esperamos que você aproveite e aprenda bastante com ela. Para mais dúvidas
-					consulte nossa documentação oficial pelo storybook.
+					Seus projetos muito mais organizados. Veja as tarefas adicionadas por seu time, por você e para você!
 				</Typography>
 			</Header>
-			<HomeSectionNotificacoes />
-			<HomeSectionDialogs />
-      <HomeSectionComponents />
+			<Typography variant="h5" textAlign={'justify'}>
+					<strong>Adicionadas Recentemente</strong>
+			</Typography>
+			
+
+			<SysFab
+				variant="extended"
+				text="Irpara Tarefas >>"
+				startIcon={<SysIcon name="add" />}
+				fixed
+				onClick={handleToTaskList}
+				
+			/>
 		</Container>
 	);
 };
+
+
 
 export default Home;
