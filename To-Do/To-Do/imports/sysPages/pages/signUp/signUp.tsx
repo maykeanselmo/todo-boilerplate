5b3fe7e6ef3952854a917284b1e7262e58e833c0
Mayke <mayke000000000@gmail.com>
2025-04-14 14:05:13 -0300

    feat: Página de singUp

--- To-Do/To-Do/imports/sysPages/pages/signUp/signUp.tsx
+++ To-Do/To-Do/imports/sysPages/pages/signUp/signUp.tsx
@@ -10,24 +10,52 @@ import TextField from '/imports/ui/components/SimpleFormFields/TextField/TextFie
 import Button from '@mui/material/Button';
 import { userprofileApi } from '../../../modules/userprofile/api/userProfileApi';
 import SimpleForm from '/imports/ui/components/SimpleForm/SimpleForm';
-
-import { signUpStyle } from './signUpStyle';
+import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import { IUserProfile } from '/imports/modules/userprofile/api/userProfileSch';
-
+import SignUpStyles from './signUpStyle';
+import SysIcon from '../../../ui/components/sysIcon/sysIcon';
+import SysFormButton from '../../../ui/components/sysFormFields/sysFormButton/sysFormButton';
 interface ISignUp {
 	showNotification: (options?: Object) => void;
 	navigate: NavigateFunction;
 	user: IUserProfile;
 }
 
+const signUpPhrases = [
+	"Organize suas ideias. Realize seus sonhos.",
+	"Tudo sob controle, no seu tempo.",
+	"Sua produtividade começa aqui.",
+	"Chega de esquecer até de lembrar!",
+	"O começo da sua rotina mais organizada começa agora.",
+	"Transforme tarefas em conquistas diárias.",
+	"Planeje hoje, conquiste amanhã.",
+	"Dê um check nas suas metas. Cadastre-se!",
+	"Você no controle da sua lista. Sempre.",
+	"Simplifique seu dia, uma tarefa de cada vez.",
+	"Menos caos, mais foco. Vamos nessa?",
+	"Listas que fazem sentido. Dias que rendem mais.",
+	"Seu tempo é precioso. Organize com propósito.",
+	"Viva o prazer de riscar uma tarefa feita!",
+	"Uma mente organizada começa com uma boa lista.",
+	"Cadastre-se e comece sua revolução produtiva!",
+	"A lista perfeita começa com um clique.",
+	"Deixe a bagunça mental no passado. Vem pro ToDo!",
+	"Porque sua rotina merece ser leve e eficiente.",
+	"Tarefas sob controle. Vida em equilíbrio.",
+  ];
+  
+
 export const SignUp = (props: ISignUp) => {
 	const { showNotification } = props;
+	const { Container, Content, FormContainer, FormWrapper } = SignUpStyles;
+	const randomPhrase = signUpPhrases[Math.floor(Math.random() * signUpPhrases.length)];
 
-	const handleSubmit = (doc: { email: string; password: string }) => {
-		const { email, password } = doc;
+	const handleSubmit = (doc: { email: string; password: string, username: string }) => {
+		const { email, password , username} = doc;
 
-		userprofileApi.insertNewUser({ email, username: email, password }, (err, r) => {
+		userprofileApi.insertNewUser({username: username ,email, password }, (err, r) => {
+			console.log("RESULTADO DA CRIAÇÃO:", { err, r });
 			if (err) {
 				console.log('Login err', err);
 				showNotification &&
@@ -37,9 +65,10 @@ export const SignUp = (props: ISignUp) => {
 						description: 'Erro ao fazer registro em nossa base de dados!'
 					});
 			} else {
+				console.log("Chamando notificação de sucesso");
 				showNotification &&
 					showNotification({
-						type: 'sucess',
+						type: 'success',
 						title: 'Cadastrado com sucesso!',
 						description: 'Registro de usuário realizado em nossa base de dados!'
 					});
@@ -47,40 +76,77 @@ export const SignUp = (props: ISignUp) => {
 		});
 	};
 
+
+	
+
 	return (
-		<Container style={signUpStyle.containerSignUp}>
-			<Box sx={signUpStyle.labelRegisterSystem}>
-				<img src="/images/wireframe/logo.png" style={signUpStyle.imageLogo} />
-				{'Cadastrar no sistema'}
+		<Container >
+			<Content>
+			<Box
+				display="flex"
+				flexDirection="column"
+				alignItems="center"
+				justifyContent="center"
+				gap={2}
+				sx={{ maxWidth: 300, mx: 'auto', mt: 4 }}
+				>
+				<Box
+					component="img"
+					src="/images/sing-up.jpg"
+					alt="Ícone de cadastro"
+					sx={{
+					width: '100%',
+					maxWidth: 200,
+					height: 'auto',
+					objectFit: 'contain',
+					}}
+				/>
+
+				<Typography variant="h6" textAlign="center">
+					{randomPhrase} 
+				</Typography>
 			</Box>
-			<SimpleForm
-				schema={{
-					email: {
-						type: String,
-						label: 'Email',
-						optional: false
-					},
-					password: {
-						type: String,
-						label: 'Senha',
-						optional: false
-					}
-				}}
-				onSubmit={handleSubmit}>
-				<TextField id="Email" label="Email" fullWidth name="email" type="email" placeholder="Digite um email" />
-				<TextField id="Senha" label="Senha" fullWidth name="password" placeholder="Digite uma senha" type="password" />
-				<Box sx={signUpStyle.containerButtonOptions}>
-					<Button color={'primary'} variant={'outlined'} id="submit">
-						Cadastrar
-					</Button>
+					<FormContainer>
+
+						<SimpleForm
+							schema={{
+								username: {
+									type: String,
+									label: 'Username',
+									optional: false
+								},
+
+								email: {
+									type: String,
+									label: 'Email',
+									optional: false
+								},
+								password: {
+									type: String,
+									label: 'Senha',
+									optional: false
+								}
+							}}
+							onSubmit={handleSubmit}>
+							<FormWrapper>
+								<TextField id="Username" label="Username" fullWidth name="username" placeholder="Digite um nome de usuário" />
+								<TextField id="Email" label="Email" fullWidth name="email" type="email" placeholder="Digite um email" />
+								<TextField id="Senha" label="Senha" fullWidth name="password" placeholder="Digite uma senha" type="password" />
+								
+								<SysFormButton variant={'outlined'} color="primary" id="submit" endIcon={<SysIcon name={'arrowForward'} />}>
+									Cadastrar
+								</SysFormButton>
+							</FormWrapper>
+
+						</SimpleForm>
+					</FormContainer>
+				<Box >
+					Já tem uma conta? Faça login clicando{' '}
+					<Link to="/signin" color={'secondary'}>
+						aqui
+					</Link>
 				</Box>
-			</SimpleForm>
-			<Box sx={signUpStyle.containerRouterSignIn}>
-				Já tem uma conta? Faça login clicando{' '}
-				<Link to="/signin" color={'secondary'}>
-					aqui
-				</Link>
-			</Box>
+			</Content>
 		</Container>
 	);
 };
--- To-Do/To-Do/imports/sysPages/pages/signUp/signUpStyle.tsx
+++ To-Do/To-Do/imports/sysPages/pages/signUp/signUpStyle.tsx
@@ -14,27 +14,73 @@
 
  */
 
-export const signUpStyle = {
-	containerSignUp: {
-		width: '100%',
-		maxWidth: 400
-	},
-	labelRegisterSystem: {
-		textAlign: 'center',
-		display: 'flex',
-		flexDirection: 'column',
-		justifyContent: 'center',
-		alignItems: 'center'
-	},
-	imageLogo: {
-		maxWidth: 100
-	},
-	containerButtonOptions: {
-		display: 'flex',
-		flexDirection: 'row',
-		justifyContent: 'space-around'
-	},
-	containerRouterSignIn: {
-		color: '#444'
-	}
-};
+ import React from 'react';
+ import Paper from '@mui/material/Paper';
+ import { styled } from '@mui/material/styles';
+ import Box from '@mui/material/Box';
+ import { sysSizing } from '../../../ui/materialui/styles';
+ 
+ interface ISignUpStyles {
+	 Container: React.ElementType;
+	 Content: React.ElementType;
+	 FormContainer: React.ElementType;
+	 FormWrapper: React.ElementType;
+ }
+ 
+ const SignUpStyles: ISignUpStyles = {
+	 Container: styled(Box)(({ theme }) => ({
+		 minHeight: '100vh',
+		 width: '100%',
+		 backgroundColor: theme.palette.primary.light,
+		 color: theme.palette.primary.contrastText,
+		 position: 'relative',
+ 
+		 [theme.breakpoints.up('md')]: {
+			 
+			 backgroundSize: 'cover',
+			 backgroundPosition: 'right'
+		 }
+	 })),
+	 Content: styled(Box)(({ theme }) => ({
+		 width: '100%',
+		 height: '100%',
+		 display: 'flex',
+		 flexDirection: 'column',
+		 justifyContent: 'center', 
+		 alignItems: 'center', 
+		 gap: theme.spacing(6),
+		 padding: `${sysSizing.spacingFixedLg} ${sysSizing.spacingFixedXl}`,
+	 
+		 [theme.breakpoints.up('md')]: {
+			 width: 'auto',
+			 height: 'auto',
+			 position: 'absolute',
+			 top: '50%',
+			 left: '50%', 
+			 transform: 'translate(-50%, -50%)' 
+		 }
+	 })),
+	 FormContainer: styled(Paper)(({ theme }) => ({
+		 width: '100%',
+		 padding: `${sysSizing.spacingFixedLg} ${sysSizing.spacingFixedXl}`,
+		 borderRadius: sysSizing.radiusLg,
+		 boxShadow: theme.shadows[3],
+		 gap: sysSizing.spacingFixedXl,
+		 display: 'flex',
+		 flexDirection: 'column',
+		 justifyContent: 'flex-start',
+		 alignItems: 'center',
+		 maxWidth: '400px'
+	 })),
+	 FormWrapper: styled(Box)(({ theme }) => ({
+		 width: '100%',
+		 display: 'flex',
+		 flexDirection: 'column',
+		 justifyContent: 'center',
+		 alignItems: 'center',
+		 gap: theme.spacing(2)
+	 }))
+ };
+ 
+ export default SignUpStyles;
+ 
